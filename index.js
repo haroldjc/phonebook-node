fae4afd6c2f2faf1e0a206e0288bf86fcba268f5
haroldjc <harold@blakzr.com>
2023-01-27 21:38:43 -0500

    Add middleware: Morgan

--- index.js
+++ index.js
@@ -1,9 +1,24 @@
 const { application } = require('express')
 const express = require('express')
 const nodemon = require('nodemon')
+const morgan = require('morgan')
 const app = express()
 
 app.use(express.json())
+
+// Morgan middleware
+morgan.token('body', (req, res) => JSON.stringify(req.body));
+
+app.use(morgan((tokens, req, res) => {
+  return [
+    tokens.method(req, res),
+    tokens.url(req, res),
+    tokens.status(req, res),
+    tokens.res(req, res, 'content-length'), '-',
+    tokens['response-time'](req, res), 'ms',
+    tokens.body(req, res)
+  ].join(' ')
+}))
 
 let persons = [
   { 
@@ -92,6 +107,8 @@ const unknownEndpoint = (request, response) => {
 }
 
 app.use(unknownEndpoint)
+
+app.use(morgan)
 
 const PORT = 3001
 app.listen(PORT, () => {
