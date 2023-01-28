ad2a97a125c6869ccb04d9842b3db3d660282499
haroldjc <harold@blakzr.com>
2023-01-28 11:48:28 -0500

    Testing mongoose and MongoDB

---
+++ mongo.js
@@ -0,0 +1,47 @@
+const e = require('express')
+const mongoose = require('mongoose')
+
+if (process.argv.length < 3) {
+  console.log('Give password as argument')
+  process.exit(1)
+}
+
+const password = process.argv[2]
+
+const url = `mongodb+srv://fullstack:${password}@cluster0.xw52nl7.mongodb.net/phonebookApp?retryWrites=true&w=majority`
+
+mongoose.set('strictQuery', false)
+mongoose.connect(url)
+
+const personSchema = new mongoose.Schema({
+  name: String,
+  number: String
+})
+
+const Person = mongoose.model('Person', personSchema)
+
+if (process.argv[3] && process.argv[4]) {
+  // Create a new Person object and save it in the DB
+  const person = new Person({
+    name: process.argv[3],
+    number: process.argv[4]
+  })
+
+  person.save()
+    .then(result => {
+      console.log(`Added ${result.name} number ${result.number} to phonebook`)
+      mongoose.connection.close()
+    })
+
+} else {
+  // List the collection of Persons
+  Person.find({})
+  .then(result => {
+    console.log('Phonebook:')
+    result.forEach(person => {
+      console.log(`${person.name} ${person.number}`)
+    })
+    mongoose.connection.close()
+  })
+
+}
