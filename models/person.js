243801f5dc516cb59d1b06502966033d47a04c7a
haroldjc <harold@blakzr.com>
2023-02-09 21:49:47 -0500

    Exercises 3.19-3.21

--- models/person.js
+++ models/person.js
@@ -15,8 +15,21 @@ mongoose.connect(url)
   })
 
 const personSchema = new mongoose.Schema({
-  name: String,
-  number: String,
+  name: {
+    type: String,
+    minLength: 3,
+    required: true,
+  },
+  number: {
+    type: String,
+    minLength: 8,
+    match: /^\d{2,3}-\d{3,}$/,
+    validate: {
+      validator: v => /\d{2,3}-\d{3,}/.test(v),
+      message: props => `${props.value} is not a valid phone number!`
+    },
+    required: true
+  },
 })
 
 personSchema.set('toJSON', {
